d83502daf6022f55314b1331df61a3c08de3f0b2
Diab haque <diab@connect.hku.hk>
2019-08-20 15:56:44 +0800

    Completed front end development

---
+++ smartbrain/src/components/Rank/Rank.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const rank =()=>{
+    return(
+        <div>
+            <div className='black f3'>
+                {'Andrei, your current rank is...'}
+            </div>
+            <div className='black f1'>
+                {'#5'}
+            </div>
+        </div>
+    )
+}
+export default rank;
